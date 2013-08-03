6816fc33579b1040fb2e3759c412ae208e00bbca
Xiaokang Wang <xiaokangwang@outlook.com>
2013-08-03 15:16:05 +0800

    Created Ping responder

---
+++ webrespond.py
@@ -0,0 +1,40 @@
+from wheezy.http import HTTPResponse
+from wheezy.http import WSGIApplication
+from wheezy.routing import url
+from wheezy.web.handlers import BaseHandler
+from wheezy.web.middleware import bootstrap_defaults
+from wheezy.web.middleware import path_routing_middleware_factory
+import .webreq
+
+
+class PingHandler(BaseHandler):
+
+    def get(self):
+        response = HTTPResponse()
+        response.write('PING_SUCCESS')
+        return response
+
+
+all_urls = [
+    url('ping', PingHandler, name='ping')
+]
+
+
+options = {}
+main = WSGIApplication(
+    middleware=[
+        bootstrap_defaults(url_mapping=all_urls),
+        path_routing_middleware_factory
+    ],
+    options=options
+)
+
+
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    try:
+        print('Sever runing')
+        make_server('', 8080, main).serve_forever()
+    except KeyboardInterrupt:
+        pass
+    print('\Killed!')
