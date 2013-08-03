b45fd5bc22c657ae530f8c5ec59323cf6194929a
Xiaokang Wang <xiaokangwang@outlook.com>
2013-08-03 21:35:18 +0800

    Finish UserID gener

---
+++ genUserID.py
@@ -0,0 +1,13 @@
+import random, string
+
+def randomword(length):
+   return ''.join(random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789") for i in range(length))
+
+def genUserID():
+    User={}
+    User["UserID"]=randomword(25)
+    User["UserSecret"]=randomword(128)
+    return User
+
+if __name__ == '__main__':
+	print(genUserID())
