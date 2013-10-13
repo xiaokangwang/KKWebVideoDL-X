d8f4131c3e1a6acab0c85393ccf3a24d9a7a81fd
Xiaokang Wang <xiaokangwang@outlook.com>
2013-10-13 15:21:10 +0800

    Finished log classify

--- logprogress.py
+++ logprogress.py
@@ -1,16 +1,30 @@
 import dbconn
 
 def AquireLogsBwt(bgn,due):
-	pass
+	return dbconn.Db_Get_LogRange(bgn,due)
+	
 
 def ClassifyLogItem(Logitem):
-	pass
+	result={}
+	resultclass=[]
+
+	for logi in Logitem:
+		if !resultclass.count(logi["Event"])
+		    resultclass.append(logi["Event"])
+
+    for insclass in resultclass:
+    	result[insclass]=[]
+
+    for logi in Logitem:
+    	result[logi["Event"]].append(logi)
+    return result
 
 def ProgressLog(bgn,due):
 	pass
 
 def MakeReport(bgn,due):
-	pass
+	OrigLog=AquireLogsBwt(bgn,due)
+	ClassifyLogItem(OrigLog)
 
 def AquireLogReport(bgn,due):
 	pass
